7e00f3798b91ec06c4acbf5b55d881b8c12e88e0
CellBody <austintingwork@gmail.com>
2016-09-29 11:14:45 +0800

    Update PostActivity

--- app/src/main/java/com/expixel/crazysub/InitApp.java
+++ app/src/main/java/com/expixel/crazysub/InitApp.java
@@ -1,6 +1,11 @@
 package com.expixel.crazysub;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 /**
  * Created by cellbody on 2016/9/23.
@@ -9,4 +14,21 @@ import android.app.Application;
 public class InitApp extends Application {
     public static final String TAG = "crazysub";
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+
+                .cacheInMemory(true)
+                // 先看會不會OOM 會的話在cach到記憶卡裡
+                //                .cacheOnDisk(true)
+                .build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
+                .defaultDisplayImageOptions(defaultOptions)
+                .build();
+        ImageLoader.getInstance().init(config);
+        Log.i(TAG, this.getClass().getSimpleName()+": ImageLoader Init");
+
+
+    }
 }
--- app/src/main/java/com/expixel/crazysub/PostActivity.java
+++ app/src/main/java/com/expixel/crazysub/PostActivity.java
@@ -2,17 +2,56 @@ package com.expixel.crazysub;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by cellbody on 2016/9/23.
  */
 
 public class PostActivity extends BaseDialogActivity {
+    @BindView(R.id.editSub)
+    EditText editSub;
+    @BindView(R.id.editDsc)
+    EditText editDsc;
+    @BindView(R.id.tvSub)
+    TextView tvSub;
+    @BindView(R.id.imgToday)
+    ImageView imgToday;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_post);
+        ButterKnife.bind(this);
+
+        ImageLoader.getInstance().displayImage("https://firebasestorage.googleapis.com/v0/b/crazysub-a7da8.appspot.com/o/cat.jpeg?alt=media&token=62a05fb1-f1be-4511-9811-948839ec75cb", imgToday);
+        editSub.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tvSub.setText(editSub.getText());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
     }
+
+
 
 
 }
